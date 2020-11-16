aa23d0f2853fa2c089a975d6e8acf43e791428fd
heyu <931680128@qq.com>
2020-11-17 00:01:55 +0800

    try push

---
+++ src/main/java/resource/UrlResource.java
@@ -0,0 +1,10 @@
+package resource;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class UrlResource implements Resource {
+    public InputStream getInputStream() throws IOException {
+        return null;
+    }
+}
